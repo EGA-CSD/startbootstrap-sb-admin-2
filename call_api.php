4f2aece2612b1b8e87c1c72e2b67e32a38335b1a
root <root@csd-linux.wisarut.com>
2018-01-17 22:40:01 +0700

    support sub api include login, getAccount, updateAccount and news

--- call_api.php
+++ call_api.php
@@ -1,4 +1,47 @@
 <?php
+
+  const SUCCESS = 0;
+  const WARNING = 1;
+  const ERROR = 2;
+  
+  ini_set('mysql.connect_timeout','5');   
+  ini_set('max_execution_time', '5');   
+  
+  function gen_response_with_json($return_code, $result_data){
+    return "{\"return_code\": ".$return_code.",\"result_data\": ".$result_data."}";
+  }
+  
+  function gen_response($return_code, $result_data){
+    $response = array('retrun_code' => $return_code, 'result_data'=> $result_data);
+    $array = array_map('htmlentities',$response);
+    $json = html_entity_decode(json_encode($array)); 
+    
+    return $json;
+  }
+  
+  function api_mysql($host, $user, $pass, $db){
+    $con = mysqli_connect($host, $user, $pass, $db, 3306);
+    
+    // Check connection
+    if (mysqli_connect_errno())
+    {
+      echo gen_response(ERROR, "Failed to connect to MySQL: " . mysqli_connect_error());
+      mysqli_close($con);
+      return ;
+    }
+    
+    $sql = "select * from uhosnet_news limit 1";
+    $result = mysqli_query($con, $sql);
+    
+    if( !$result ){
+      echo gen_response(WARNING, "no result" );
+    }else{
+      $dataRow = $result->fetch_array(MYSQLI_ASSOC);
+      echo gen_response_with_json(SUCCESS, json_encode($dataRow));
+    }
+    
+    mysqli_close($con);
+  }
   
   function api_get( $url, $headers ){
     $ch = curl_init($url);
@@ -12,9 +55,9 @@
     curl_close($ch);
     
     if( $result == NULL ){
-      echo "service unavialable";
+      echo gen_response(ERROR, "service unavialable");
     }else{
-      echo $result; 
+      echo gen_response_with_json(SUCCESS, json_encode($result)); 
     }
     
   }
@@ -30,10 +73,11 @@
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     $result = curl_exec($ch);
     curl_close($ch);
+    
     if( $result == NULL ){
-      echo "service unavialable";
+      echo gen_response(ERROR, "service unavialable");
     }else{
-      echo $result; 
+      echo gen_response_with_json(SUCCESS, json_encode($result)); 
     }
   }
   
@@ -48,10 +92,11 @@
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     $result = curl_exec($ch);
     curl_close($ch);
+    
     if( $result == NULL ){
-      echo "service unavialable";
+      echo gen_response(ERROR, "service unavialable");
     }else{
-      echo $result; 
+      echo gen_response_with_json(SUCCESS, json_encode($result)); 
     }
   }
   
@@ -59,14 +104,22 @@
   $method = $_POST['method'];
 	$udata = $_POST['data'];
   $url = $_POST['url'];
- 
+
+  // Main Function
   if( $method == "GET" ){
     api_get($url, $headers);
   }else if( $method == "POST" ){
     api_post($url, $udata, $headers);
   }else if( $method == "PUT"){
     api_put($url, $udata, $headers);
+  }else if( $method == "DB"){
+    $host = "10.111.8.74";
+    $user = "uhosnet";
+    $pass = "uhosnet";
+    $db = "uhosnet_db";
+    api_mysql($host, $user, $pass, $db);
   }else{
-    echo "Not support this method : ".$method;
+    echo gen_response(SUCCESS, "Not support this method : ".$method );
   }
+
 ?>
